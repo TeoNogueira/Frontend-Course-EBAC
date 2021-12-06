f36019766bf35474724ca9209deea8f98f43f060
Teo Nogueira <teonogueira17@gmail.com>
2021-12-06 18:44:44 -0300

    module_19A_callbacks

---
+++ MODULO 19A/assets/libs/jquery/scripts.js
@@ -0,0 +1,76 @@
+
+// instancia jQuery e evita conflitos;
+
+$(document).ready(function($){
+
+    $('.owl-carousel').owlCarousel();
+
+    let titulos = $('h4') // selecionar tag
+    let itens = $('.featured-item') // '' '' class
+    let destaques = $('#featured') // '' '' id
+    // console.log(titulos.first())
+
+//  let line = $('.line-dec')
+    $('.featured-item a').addClass('btn btn-dark stretch-link');
+//     $('.section-heading h1').addClass('active-h1');
+       $('.featured-item a').click(function(){
+
+        $(this).css({
+
+            'color': '#ff0',
+            // 'background': '#ff0',
+            'font-weight': '100',
+        })
+
+       }) 
+
+
+
+
+// $('h4').text('Bem-vindo a loja')
+
+
+//manipulação de evento
+
+
+// $('.featured-item: ').on('click', function(event) {
+
+//     event.preventDefault();
+
+//     alert('Produto esgotado');
+// });
+
+
+
+
+// hide
+// show()
+//fadeIn(2000) transiçao/entrada
+//fadeOut()     ''/saida
+
+// $('.featured-item a').css('color', '#fafafa')
+/* $('.featured-item a').css({
+'color': '#fff',
+'background': '#bbbbaa',
+instancia por objeto
+})*/
+
+
+
+// CALLBACKS:
+/*
+Entendendo ações que começam ao termino de outro
+*/
+
+$('.featured-item:nth(0)').hide(2000, function() {
+
+//callback
+
+console.log($(this).find('h4').text() + ' esgotado') 
+})
+.show(2000, function() {
+
+    console.log($(this).find('h4').text() + ' Em estoque!') 
+
+})
+})
