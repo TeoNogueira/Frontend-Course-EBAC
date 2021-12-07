7e1059b53df416a1206608a06a605e1194aae17c
Teo Nogueira <teonogueira17@gmail.com>
2021-12-07 17:26:59 -0300

    colocar_data_target_nos_links_contatos_e_sobre_finished_19A

--- MODULO 19A/assets/libs/jquery/scripts.js
+++ MODULO 19A/assets/libs/jquery/scripts.js
@@ -58,12 +58,35 @@ $('#form-submit').on('click', function(evt) {
             transition: '0.5s',
             opacity: '0.5',
          
-            // opacity: 'toggle'           
-          
+            // opacity: 'toggle'                    
 
         })
 
     }
+
+
+    /*
+    * Ouvinte de eventos .nav
+     */
+
+    $('.nav-modal-open').on('click', function(e) {
+
+        e.preventDefault();
+
+        let elem = $(this).attr('rel'); 
+        // $('.modal-body').html($('#'+elem).html()) 
+        $('.modal-body').html($('#'+elem).html())
+
+        $('.modal-header h5.modal-title ').html($(this).text)
+
+        let myModal = new bootstrap.Modal($('#modelId'))
+
+            myModal.show()
+        
+
+       
+
+    });
 
 
 })
