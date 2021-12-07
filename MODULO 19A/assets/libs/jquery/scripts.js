55e90e54fedad411bb1de798e3670ff852a179f9
Teo Nogueira <teonogueira17@gmail.com>
2021-12-07 17:45:20 -0300

    modify_line_248_contato_visually-hidden-deleted

--- MODULO 19A/assets/libs/jquery/scripts.js
+++ MODULO 19A/assets/libs/jquery/scripts.js
@@ -89,4 +89,26 @@ $('#form-submit').on('click', function(evt) {
     });
 
 
+    // about
+
+    $('.nav-modal-open').on('click', function(e) {
+
+        e.preventDefault();
+
+        let elem = $(this).attr('rel'); 
+        // $('.modal-body').html($('#'+elem).html()) 
+        $('.modal-body').html($('#'+elem).html())
+
+        $('.modal-header h5.modal-title ').html($(this).text)
+
+        let myModal = new bootstrap.Modal($('#modelId'))
+
+            myModal.show()
+        
+
+       
+
+    });
+
+
 })
